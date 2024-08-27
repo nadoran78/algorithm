43651fa03ab59df5b8e746aaa7bdd528d64fad03
nadoran78 <141199772+nadoran78@users.noreply.github.com>
2024-08-27 14:30:42 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 5884.61 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -3,20 +3,51 @@ import java.util.*;
 class Solution {
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
         long answer = 0;
+
+        int deliverySum = Arrays.stream(deliveries).sum();
+        int pickupSum = Arrays.stream(pickups).sum();
         
-        int deliver = 0;
-        int pickup = 0;
-        for (int i = n - 1; i >= 0; i--) {
-            if (deliveries[i] > 0 || pickups[i] > 0) {
-                int cnt = 0;
-                while (deliver < deliveries[i] || pickup < pickups[i]) {
-                    cnt++;
-                    deliver += cap;
-                    pickup += cap;
+        int lastIdx = n - 1;
+
+        while (deliverySum != 0 || pickupSum != 0) {
+            boolean isMostLong = true;
+
+            int delivery = Math.min(cap, deliverySum);
+            int pickup = Math.min(cap, pickupSum);
+
+            deliverySum -= delivery;
+            pickupSum -= pickup;
+
+            for (int i = lastIdx; i >= 0; i--) {
+                if (deliveries[i] == 0 && pickups[i] == 0) {
+                    continue;
                 }
-                deliver -= deliveries[i];
-                pickup -= pickups[i];
-                answer += (i + 1) * cnt * 2;
+
+                if (isMostLong) {
+                    answer += (i + 1) * 2L;
+                    isMostLong = false;
+                    lastIdx = i;
+                }
+
+                if (deliveries[i] >= delivery) {
+                    deliveries[i] = deliveries[i] - delivery;
+                    delivery = 0;
+                } else {
+                    delivery -= deliveries[i];
+                    deliveries[i] = 0;
+                }
+
+                if (pickups[i] >= pickup) {
+                    pickups[i] = pickups[i] - pickup;
+                    pickup = 0;
+                } else {
+                    pickup -= pickups[i];
+                    pickups[i] = 0;
+                }
+
+                if (pickup == 0 && delivery == 0) {
+                    break;
+                }
             }
         }
         return answer;
