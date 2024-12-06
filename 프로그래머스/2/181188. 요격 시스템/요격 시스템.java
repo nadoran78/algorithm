27dbc2c6542198bacad687ff32aeca3a377e998f
nadoran78 <141199772+nadoran78@users.noreply.github.com>
2024-12-06 10:51:06 +0900

    [level 2] Title: 요격 시스템, Time: 364.61 ms, Memory: 183 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -3,15 +3,24 @@ import java.util.*;
 class Solution {
     public int solution(int[][] targets) {
         int answer = 0;
-        Arrays.sort(targets, (o1, o2) -> o1[1] - o2[1]);
         
-        int pre = -1;
-        for (int[] target : targets) {
-            if (pre <= target[0]) {
-                answer++;
-                pre = target[1];
-            }
+        PriorityQueue<int[]> pq = new PriorityQueue<>((x, y) -> y[1] - x[1]);
+        
+        for (int[] target: targets) {
+            pq.offer(target);
         }
+        
+        while (!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            int maxStart = cur[0];
+            
+            while (!pq.isEmpty() && pq.peek()[1] > maxStart) {
+                maxStart = Math.max(pq.poll()[0], maxStart);
+            }
+            
+            answer++;
+        }
+        
         return answer;
     }
 }
