f27c192dc3ff9a91446fcfa991d35411d3b0e1de
nadoran78 <141199772+nadoran78@users.noreply.github.com>
2024-12-16 17:48:34 +0900

    [level 2] Title: 점 찍기, Time: 17.53 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.java
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -1,13 +1,14 @@
 class Solution {
     public long solution(int k, int d) {
         long answer = 0;
-
+        
         for (int i = 0; i <= d; i += k) {
+            double y = Math.sqrt(Math.pow(d, 2) - Math.pow(i, 2));
             
-            int y = (int) Math.sqrt((long) d * d - (long) i * i);
-            answer += (y / k) + 1;
+            int count = (int) (y / k) + 1;
+            
+            answer += (long) count;
         }
-
         return answer;
     }
 }
