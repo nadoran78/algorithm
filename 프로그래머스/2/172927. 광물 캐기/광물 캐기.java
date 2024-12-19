d15209af54b2ca0b95c0fe93ffe0ba7dd3d24166
nadoran78 <141199772+nadoran78@users.noreply.github.com>
2024-12-19 10:57:21 +0900

    [level 2] Title: 광물 캐기, Time: 392.42 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -1,57 +1,66 @@
 import java.util.*;
 
 class Solution {
-    int[] pickOrder;
-    int[] mineralNumbers;
-    int[][] fatigue;
+    int[] pickResult;
+    int[][] tired;
     int answer = Integer.MAX_VALUE;
     public int solution(int[] picks, String[] minerals) {
         
-        fatigue = new int[][]{{1, 1, 1}, {5, 1, 1}, {25, 5, 1}};
+        int picksTotal = Arrays.stream(picks).sum();
         
-        pickOrder = new int[Arrays.stream(picks).sum()];
-        mineralNumbers = new int[minerals.length];
-        for (int i = 0; i < minerals.length; i++) {
-            String mineral = minerals[i];
-            if (mineral.equals("diamond")) {
-                mineralNumbers[i] = 0;
-            } else if (mineral.equals("iron")) {
-                mineralNumbers[i] = 1;
-            } else if (mineral.equals("stone")) {
-                mineralNumbers[i] = 2;
-            }
-        }
+        tired = new int[][]{{1, 1, 1}, {5, 1, 1}, {25, 5, 1}};
         
-        dfs(picks, 0);
+        pickResult = new int[picksTotal];
+        
+        dfs(picks, minerals, 0);
         
         return answer;
     }
     
-    private void dfs(int[] picks, int depth) {
-        if (depth == pickOrder.length) {
-            int sum = 0;
-            int idx = 0;
-            for (int pick : pickOrder) {
-                if (idx >= mineralNumbers.length) {
-                    continue;
-                }
-                
-                for (int i = idx; i < Math.min(idx + 5, mineralNumbers.length); i++) {
-                    sum += fatigue[pick][mineralNumbers[i]];
-                }
-                idx += 5;
-            }
-            answer = Math.min(sum, answer);
+    private void dfs(int[] picks, String[] minerals, int depth) {
+        if (depth == pickResult.length) {
+            updateTired(minerals);
             return;
         }
         
-        for (int i = 0; i < picks.length; i++) {
+        for (int i = 0; i < 3; i++) {
             if (picks[i] > 0) {
-                pickOrder[depth] = i;
-                picks[i]--;
-                dfs(picks, depth + 1);
-                picks[i]++;
+                int[] newArray = Arrays.copyOfRange(picks, 0, picks.length);
+                newArray[i]--;
+                pickResult[depth] = i;
+                dfs(newArray, minerals, depth + 1);
             }
         }
+    }
+    
+    private void updateTired(String[] minerals) {
+        int totalTired = 0;
+        
+        int pickIdx = 0;
+        int mineralIdx = 0;
+        while(pickIdx < pickResult.length && mineralIdx < minerals.length) {
+            int pick = pickResult[pickIdx];
+            
+            int count = 0;
+            while (mineralIdx < minerals.length && count < 5) {
+                String mineral = minerals[mineralIdx];
+                int mineralNumber = 0;
+                if (mineral.equals("diamond")) {
+                    mineralNumber = 0;
+                } else if (mineral.equals("iron")) {
+                    mineralNumber = 1;
+                } else if (mineral.equals("stone")) {
+                    mineralNumber = 2;
+                }
+                
+                totalTired += tired[pick][mineralNumber];
+                
+                mineralIdx++;
+                count++;
+            }
+            pickIdx++;
+        }
+        
+        answer = Math.min(answer, totalTired);
     }
 }
