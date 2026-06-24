c2c4bb25826c1820f889fdb21638bfd98bccc8df
nadoran78 <141199772+nadoran78@users.noreply.github.com>
2026-06-25 00:29:06 +0900

    [level 1] Title: 과일로 만든 아이스크림 고르기, Time: , Memory: undefined -BaekjoonHub

---
+++ MySQL/프로그래머스/1/133025. 과일로 만든 아이스크림 고르기/과일로 만든 아이스크림 고르기.sql
@@ -0,0 +1,8 @@
+-- 코드를 입력하세요
+SELECT fh.FLAVOR
+from FIRST_HALF fh
+left join ICECREAM_INFO ii
+on fh.FLAVOR = ii.FLAVOR
+where fh.TOTAL_ORDER > 3000
+    and ii.INGREDIENT_TYPE = 'fruit_based'
+order by fh.TOTAL_ORDER desc;
