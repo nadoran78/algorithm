cd0237864e31a0297fd95826bb7f581f12752c39
이정창 | Lee Jung-Chang <141199772+nadoran78@users.noreply.github.com>
2026-09-15 00:04:02 +0900

    [level 2] Title: 전화번호 목록, Time: 242.92 ms, Memory: 110 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);
+        
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            String phone = phone_book[i];
+            
+            if (phone_book[i + 1].startsWith(phone)) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
